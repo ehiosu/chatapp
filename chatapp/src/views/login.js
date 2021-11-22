d41f30d98d22b5722d5c27c8f9d6431deab2feef
ehiosu <53658556+ehiosu@users.noreply.github.com>
2021-11-22 21:42:06 +0100

    Login v.1

---
+++ chatapp/src/views/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import firebase from '../frebase'
+function login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+// function signin(){
+//     firebase.auth().create
+// }
+
+export default login
