a9f754213eafcf814f4b65f458685e63f79541eb
ehiosu <53658556+ehiosu@users.noreply.github.com>
2021-11-22 21:47:25 +0100

    Update login.js

--- chatapp/src/views/login.js
+++ chatapp/src/views/login.js
@@ -3,7 +3,11 @@ import firebase from '../frebase'
 function login() {
     return (
         <div>
-            
+            <div className="row">
+                <div className="col-lg-4 card mx-auto">
+                    <h1>ehi</h1>
+                </div>
+            </div>
         </div>
     )
 }
