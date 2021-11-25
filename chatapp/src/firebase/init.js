0794c62447fd46015fcf61c003f9e59c2e182e0f
ehiosu <53658556+ehiosu@users.noreply.github.com>
2021-11-25 21:03:23 +0100

    login + firebase

--- chatapp/src/firebase/init.js
+++ chatapp/src/firebase/init.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebase from 'firebase'
- import   'firebase/firestore'
-    import 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { Firestore } from '@firebase/firestore'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,13 +18,17 @@ const firebaseConfig = {
   appId: "1:964119153742:web:f79253349d4d97ece5c71f",
   measurementId: "G-V8316EN6XY"
 };
- 
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
- const app = firebase.initializeApp(firebaseConfig);
-  app.firestore().settings({timestampsInSnapshots : true,merge: true})
-   
-
-  export default app
+const app =initializeApp(firebaseConfig);
+const auth = getAuth();
 // const analytics = getAnalytics(app);
+
+export  function signup(email,password){
+  return createUserWithEmailAndPassword(auth,email,password).then((userCredential) => {
+    // Signed in 
+    // const user = userCredential.user;
+    alert("created")
+    // ...
+  })
+}
---
+++ chatapp/src/views/login.js
@@ -0,0 +1,64 @@
+<<<<<<< Updated upstream
+=======
+import { useRef } from 'react';
+import React from 'react';
+import {signup}  from '../firebase/init';
+// import { Auth as auth } from '@firebase/auth'
+function Login()
+ {
+    const emailref = useRef();
+    const passwordref = useRef();
+
+
+
+async function createaccount(){
+try{
+    await signup(emailref.current.value,passwordref.current.value)
+}catch{
+    alert("error")
+}
+}
+
+
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-lg-4 card mx-auto mt-5 shadow">
+                    <div className="form-group px-5 py-5">
+                        <h2 className="lead text-center fs-2">Create Account</h2>
+                        <form className="form-group ">
+                          <div className="row row-cols-1 my-4">
+                                <input type="email" className="form-control" placeholder="abc@xyz.com" ref={emailref}></input>
+                          </div>
+                           <div className="row row-cols-1 my-4 ">
+                                <input type="password" placeholder="Password" className="form-control"aria-describedby="passwordHelpBlock"ref={passwordref} ></input>
+                                <div id="passwordHelpBlock" class="form-text">
+                                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                                </div>
+                          </div>
+                           <div className="row row-cols-1 my-4 ">
+                                <input type="password" placeholder="Password" className="form-control"aria-describedby="passwordHelpBlock2" ></input>
+                                <div id="passwordHelpBlock2" class="form-text text-start">
+                                   Confirm Password.
+                                </div>
+                          </div>
+                          
+                            <div class="d-grid gap-2 row row-cols-1">
+                                <button class="btn btn-light" type="button" onClick={createaccount}>Login</button>
+                            </div>
+                        </form>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+// function signin(){
+//     firebase.auth().create
+// }
+
+export default Login
+>>>>>>> Stashed changes
